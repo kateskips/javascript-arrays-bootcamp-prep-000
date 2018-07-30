c187ff71aff25315f3b7538e81ebef732c0cd661
kateskips <follett.katie@gmail.com>
2018-07-30 18:37:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -39,3 +39,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length - 1);
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  
+}
